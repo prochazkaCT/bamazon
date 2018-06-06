04a8c76883217cdc8063435ab1c3b5db0a5bd5a7
prochazkaCT <sharon@countertools.org>
2018-06-06 11:39:14 -0400

    Final

--- bamazon.sql
+++ bamazon.sql
@@ -12,7 +12,7 @@ CREATE TABLE products (
     stock_quantity INTEGER (4) NOT NULL
 );
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Lavender Body Lotion", "Bath and Body", 6.75, 12), ("Bordeaux Fig and Vetiver Candle", "Aromatherapy Candles", 20.00, 14), ("Emerald and Vetiver Candle", "Aromatherapy Candles", 17.25, 8), ("Lavender Essential Bath Oil", "Bath and Body", 13.50, 5), ("Bath Mitt", "Personal Care", 5.25, 2), ("Pure Olive Oil Soap", "Bath and Body", 4.75, 8), ("Bath Bombs Variety Set", "Bathing Accessories", 18.95, 20), ("Body Brush", "Bathing Accessories", 9.95, 12), ("Ancient Minerals Magnesium Bath Flakes", "Bath and Body", 29.00, 10), ("Argan Oil", "Hair Care Products", 10.71, 15), ("Safe Sunscreen", "Sunscreen Products", 9.40, 5);
 
 SELECT * FROM products; 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table2');
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -7,47 +8,97 @@ var connection = mysql.createConnection({
   port: 3306,
 
   // Your username
-  user: "PublicUse",
+  user: "",
 
   // Your password
-  password: "WeakPass",
+  password: "",
   database: "bamazon_db"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  fetchProducts();
+  showTable();
 });
 
-function executeQuery(options, cb) {
-  if(!options || !options.sql)
-    throw (new Error('Invalid sql statement')); 
-  db.query(options.sql, function (err, result, fields) {
-              if (err) throw err;
-              cb(result);
-          });
-}
+//Function to show the table in the command line
+function showTable () {
+  connection.query("SELECT * FROM products", function (err, res) {
+    var table = new Table({
+    head: ['Item #', 'Product', 'Department', 'Price ($)', 'Available']
+    , colWidths: [10, 40, 30, 13, 13]
+    });
+    for (var i = 0; i < res.length; i++) {
+      table.push(
+        [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+      );
+    }
+  console.log(table.toString());
+  forSale();
+  });
+};
 
-function fetchProducts(res){
-  var options = {
-     sql : 'SELECT * FROM products'
-  }
-  executeQuery(options, function(result){
-  res.write("<table>");
-  res.write("<tr>");
-  for(var column in result[0]){
-      res.write("<td><label>" + column + "</label></td>");
-  }
-  res.write("</tr>");
-  for(var row in result){
-      res.write("<tr>");
-      for(var column in result[row]){
-          res.write("<td><label>" + result[row][column] + "</label></td>");       
+//Function that starts the inquirer prompt
+function forSale(){
+  inquirer
+    .prompt([
+      {
+        name: "itemID",
+        type: "input",
+        message: "Which item would you like to purchase? Please enter the item number from the first column : ", 
+        validate: function (input) {
+          if (isNaN(input) === false && input !== '' && input <= 11)  {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      },     
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to purchase?",
+        validate: function (input) {
+          if (isNaN(input) === false && input !== '') {
+            return true;
+          } else {
+          return false;
+          }
+        }
       }
-      res.write("</tr>");         
-  }
-  res.write("</table>");
-});
-}
+    ]).then(function(promptResponse){
+      var itemID = promptResponse.itemID;
+      var quantity = promptResponse.quantity;
+      
+      var query = connection.query("SELECT * FROM products WHERE item_id=?",[itemID], function (err, res) {
+        // console.log("The item search query is: " + query.sql);
+        if (err) throw err;
 
+        if (quantity > res[0].stock_quantity ) {
+          console.log("Sorry, not enough of those items are available at the moment. Hope you can settle on something else!"); 
+          forSale();
+        } else {
+          console.log("Your order has been received and will be fullfilled.");
+          var userCost = quantity * res[0].price;
+          console.log("The total cost to be charged to your account is: " + userCost.toFixed(2));
+          var stockUpdate = res[0].stock_quantity - quantity;
+          console.log("There are only " + stockUpdate + " left in stock - hurry back for more!")
+          updateSQLtable(stockUpdate, itemID);
+        }
+      })
+    });
+  };
+
+//Function to update the SQL table 
+function updateSQLtable (stockUpdate, itemID) {
+  console.log("Updating stock");
+  var updateTable = ` 
+  UPDATE products
+  SET stock_quantity = ${stockUpdate}
+  WHERE item_id = ${itemID};`;
+
+  connection.query(updateTable, function (err, res) {
+    connection.end();
+  });
+  console.log("Product updated!");
+}
